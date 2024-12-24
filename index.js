1e33bba5d2f93d55a5869a228faa171b0b987d41
techswarn <tech.swarns@gmail.com>
2024-12-24 13:48:33 +0530

    code update

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ fastify.get('/', async function handler (request, reply) {
 
 // Run the server!
 try {
-  await fastify.listen({ port: 3000 })
+  await fastify.listen({ port: 3000, host: '0.0.0.0' })
 } catch (err) {
   fastify.log.error(err)
   process.exit(1)
